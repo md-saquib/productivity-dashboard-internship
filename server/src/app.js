abb934b30ec6ad217d35b0418b76a37acb180a57
saquib <ahmed.saquib.8910@gmail.com>
2026-09-22 23:10:16 +0530

    successfully hosted client site and added url to CLIENT_ORIGIN

--- server/src/app.js
+++ server/src/app.js
@@ -15,7 +15,7 @@ app.set('trust proxy', 1);
 
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: config.CLIENT_ORIGIN,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
